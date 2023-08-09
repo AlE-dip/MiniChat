362fc969894d5dbd502f74e04ce1ffbe69758d6c
Linh <71812422+AlE-dip@users.noreply.github.com>
2023-08-09 22:30:14 +0700

    create project

---
+++ MiniChat/src/test/java/com/ale/minichat/MiniChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ale.minichat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
